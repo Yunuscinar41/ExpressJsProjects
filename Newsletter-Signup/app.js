826432ddceb2cbe5a53cf0d0f7fe688d1f070e9b
Yunus Emre Çınar <www.yunuscinar4141@gmail.com>
2022-09-04 14:58:50 +0300

    SignUpPage

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  console.log(req.body);
+
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: fname,
+          LNAME: lname,
+        },
+      },
+    ],
+  };
+
+  const url = "https://us13.api.mailchimp.com/3.0/lists/36e5aac1a7";
+
+  const jsonData = JSON.stringify(data);
+
+  const options = {
+    method: "POST",
+    auth: "Yunus:103c877b72647d9631cae671017085d8-us13",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("on", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("The server is running on port 3000");
+});
+
+//API KEY
+// 103c877b72647d9631cae671017085d8-us13
+//LIST ID
+// 36e5aac1a7
